5f282aa9c383661b5ceaf7c1b04e71751f56a859
chul0721 <chul070721@naver.com>
2021-06-20 11:52:07 +0900

    refactor: component

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import tailwind from 'tailwind-rn'
+
+export default function Header() {
+  return (
+    <View>
+      <Text>Header Component!</Text>
+    </View>
+  )
+}
